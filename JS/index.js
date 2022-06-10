ab26345e99d6d5965b5112d7cb930ecb862487fc
JHONATA BATISTA SANTOS <jho.bat9@gmail.com>
2022-06-10 10:50:59 -0300

    a

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+let email = document.getElementById("email");
+
+
+
+function dataNascMinima(){
+    let hoje = new Date()
+    let dia = String(hoje.getDate()).padStart(2,'0')
+    let mes = String(hoje.getMonth()+1).padStart(2,'0')
+    let ano = hoje.getFullYear()
+    return `${ano-16}-${mes}-${dia}`
+}
+let data = document.querySelector(`input[type="date"]`) 
+data.value = dataNascMinima()
